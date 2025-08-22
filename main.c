ff8d96297d0d001428dbfe719f4cb622cf12bf30
Cainan Schiavolin <cainanloyola376@gmail.com>
2025-08-22 01:59:57 -0300

    Added more comments

--- main.c
+++ main.c
@@ -18,8 +18,6 @@ int iterativeBinarySearch(int *arr, int tam, int search);
 int recursiveBinarySearch(int *arr, int size, int search);
 
 int main() {
-    srand(time(NULL));
-
     unsigned int numElements;
     scanf("%u", &numElements);
 
@@ -29,6 +27,8 @@ int main() {
         scanf("%d", &arr[i]);
     
     int target, found;
+    // Mantém um loop para realizar as ações do usuário
+    // Digitar 5 limpa a memória e encerra o programa
     while (1) {
         int option;
         scanf("%d", &option);
@@ -79,62 +79,6 @@ void printArray(int *arr, int size) {
     printf("%d\n", arr[size - 1]);
 }
 
-//
-// Funções de busca
-//
-
-int sequentialSearch(int *arr, int size, int search) {
-    for (int i = 0; i < size; i++)
-        if (arr[i] == search)
-            return i;
-
-    return -1;
-}
-
-int iterativeBinarySearch(int *arr, int size, int search) {
-    int start = 0;
-    int end = size - 1;
-
-    // verifica se o programa já dividiu o suficiente para achar o número
-    while (start <= end) {
-        int mid = (start + end) / 2;
-
-        if (arr[mid] == search)
-            return mid;
-        else if (arr[mid] > search)
-            end = mid - 1;
-        else
-            start = mid + 1;
-    }
-
-    return -1;
-}
-
-// busca binária recursiva; mantemos o padrão de 3 parâmetros por função
-int recursiveBinarySearch(int *arr, int size, int search) {
-    int end = size - 1; // o final sempre é o tamanho - 1
-    int start = 0; // o começo ser sempre igual a 0 se torna um problema com apenas 3 paramêtros (inicialmente)
-
-    if (start > end) return -1;
-
-    int mid = (start + end) / 2;
-
-    if (arr[mid] == search) return mid;
-
-    if (arr[mid] > search) {
-        // o "novo tamanho (size)" é metade do atual
-        int idx = recursiveBinarySearch(arr, size / 2, search);
-        return idx;
-    } else if (arr[mid] < search) {
-        // levando em consideração o começo sempre ser 0, atualizamos o array (que nesse caso é considerado o pointer do primeiro elemento), para que possua o endereço do "novo primeiro elemento",
-        // o qual será 1 após o meio (mid + 1). portanto, o que era arr[mid + 1] agora será equivalente a arr[0].
-        arr = arr + (mid + 1);
-        int idx = recursiveBinarySearch(arr, size / 2, search); 
-        int diff = (mid + 1); // adicionamos a diferença causada pela atualização anterior do array no index retornado
-        return idx + diff;
-    }
-}
-
 // Função que retorna um novo array, contendo os elementos em ordem invertida do array passado por parâmetro
 int* invertArray(int* arr, int size) {
     int *inverted = (int*) malloc(size * sizeof(int));
@@ -143,4 +87,63 @@ int* invertArray(int* arr, int size) {
         inverted[i] = arr[size - i - 1];
 
     return inverted;
+}
+
+//
+// Funções de busca
+//
+
+// Passa item por item do array em busca do target
+int sequentialSearch(int *arr, int size, int target) {
+    for (int i = 0; i < size; i++)
+        if (arr[i] == target)
+            return i;
+
+    return -1;
+}
+
+int iterativeBinarySearch(int *arr, int size, int target) {
+    int start = 0;
+    int end = size - 1;
+
+    // verifica se o programa já dividiu o suficiente para achar o número
+    while (start <= end) {
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        // Se for maior, começa de novo da metade pra baixo
+        else if (arr[mid] > target)
+            end = mid - 1;
+        // se não, da metade pra cima
+        else
+            start = mid + 1;
+    }
+
+    return -1;
+}
+
+// busca binária recursiva; mantemos o padrão de 3 parâmetros por função
+int recursiveBinarySearch(int *arr, int size, int target) {
+    int end = size - 1; // o final sempre é o tamanho - 1
+    int start = 0; // o começo ser sempre igual a 0 se torna um problema com apenas 3 paramêtros (inicialmente)
+
+    if (start > end) return -1;
+
+    int mid = (start + end) / 2;
+
+    if (arr[mid] == target) return mid;
+
+    if (arr[mid] > target) {
+        // o "novo tamanho (size)" é metade do atual
+        int idx = recursiveBinarySearch(arr, size / 2, target);
+        return idx;
+    } else if (arr[mid] < target) {
+        // levando em consideração o começo sempre ser 0, atualizamos o array (que nesse caso é considerado o pointer do primeiro elemento), para que possua o endereço do "novo primeiro elemento",
+        // o qual será 1 após o meio (mid + 1). portanto, o que era arr[mid + 1] agora será equivalente a arr[0].
+        arr = arr + (mid + 1);
+        int idx = recursiveBinarySearch(arr, size / 2, target); 
+        int diff = (mid + 1); // adicionamos a diferença causada pela atualização anterior do array no index retornado
+        return idx + diff;
+    }
 }
